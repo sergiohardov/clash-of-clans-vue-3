5dab13a5292cfe44dda90869f683e85fc246d2a4
sergiohardov <sergiohardov@gmail.com>
2022-04-25 20:29:31 +0300

    add & connect images

--- src/components/UI/TheCard.vue
+++ src/components/UI/TheCard.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="card">
+    <div v-if="imgUrl" class="card-img__wrapper">
+      <img :src="imgUrl" :alt="title" class="card-img" />
+    </div>
     <span class="card-name">{{ name }}</span>
     <span class="card-title">{{ title }}</span>
     <div class="card-body">
@@ -18,6 +21,9 @@ export default {
     title: {
       type: String,
       required: true,
+    },
+    imgUrl: {
+      type: String,
     },
   },
 };
--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="md-body">
     <div class="card__wrapper" v-for="item in items" :key="item.id">
-      <TheCard :name="`${item.lvl} lvl`" :title="item.title">
+      <TheCard :name="`${item.lvl} lvl`" :title="item.title" :imgUrl="item.img">
         {{ item.desc }}
       </TheCard>
     </div>
@@ -19,12 +19,14 @@ export default {
           id: 1,
           title: "Archer",
           desc: "archer archer archer",
+          img: require("@/assets/img/archer.png"),
           lvl: 4,
         },
         {
           id: 2,
           title: "Wizard",
           desc: "Wizard Wizard arcWizarder",
+          img: require("@/assets/img/wizard.png"),
           lvl: 5,
         },
       ],
