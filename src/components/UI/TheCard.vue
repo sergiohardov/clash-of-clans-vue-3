7d571a6cd3d46f204a8173d5fc14c470ad02c976
sergiohardov <sergiohardov@gmail.com>
2022-04-25 19:56:16 +0300

    create component card

---
+++ src/components/UI/TheCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <span class="card-name">{{ name }}</span>
+    <span class="card-title">{{ title }}</span>
+    <div class="card-body">card body</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+t
--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -1,3 +1,31 @@
 <template>
   <h2>This is home</h2>
+  <div class="card__wrapper" v-for="item in items" :key="item.id">
+    <TheCard :name="`${item.lvl} lvl`" :title="item.title" />
+  </div>
 </template>
+
+<script>
+import TheCard from "@/components/UI/TheCard";
+export default {
+  components: { TheCard },
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          title: "Archer",
+          desc: "archer archer archer",
+          lvl: 4,
+        },
+        {
+          id: 2,
+          title: "Wizard",
+          desc: "Wizard Wizard arcWizarder",
+          lvl: 5,
+        },
+      ],
+    };
+  },
+};
+</script>
